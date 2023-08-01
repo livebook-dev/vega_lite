518d1d13eba4aa1fbb135129ab1c680b4c47ab93
Cristine Guadelupe <cristineguadelupe@me.com>
2023-08-01 19:24:58 +0700

    Data module (#49)
    Co-authored-by: José Valim <jose.valim@gmail.com>

---
+++ lib/vega_lite/data.ex
@@ -0,0 +1,231 @@
+defmodule VegaLite.Data do
+  @moduledoc """
+  Data is a VegaLite module designed to provide a shorthand API for commonly used charts and
+  high-level abstractions for specialized plots.
+
+  Optionally accepts and always returns a valid `VegaLite` spec, fostering flexibility to be used
+  alone or in combination with the `VegaLite` module at any level and at any point.
+
+  It relies on internal type inference, and although all options can be overridden,
+  only data that implements the [Table.Reader](https://hexdocs.pm/table/Table.Reader.html)
+  protocol is supported.
+  """
+
+  alias VegaLite, as: Vl
+
+  @doc """
+  Returns the specification for a given data and a list of fields to be encoded.
+
+  See `chart/3`.
+  """
+  @spec chart(Table.Reader.t(), keyword()) :: VegaLite.t()
+  def chart(data, fields), do: chart(Vl.new(), data, fields)
+
+  @doc """
+  Returns the specification for the a given data, a mark and a list of fields to be encoded.
+
+  Each argument that is not a `VegaLite` specification nor a data is accepted as the argument
+  itself or a keyword list of options. All options must follow the specifications of the
+  `VegaLite` module.
+
+  ## Examples
+
+      data = [
+        %{"category" => "A", "score" => 28},
+        %{"category" => "B", "score" => 55}
+      ]
+
+      Data.chart(data, :bar, x: "category", y: "score")
+
+  The above examples achieves the same results as the example below.
+
+      Vl.new()
+      |> Vl.data_from_values(data, only: ["category", "score"])
+      |> Vl.mark(:bar)
+      |> Vl.encode_field(:x, "category", type: :nominal)
+      |> Vl.encode_field(:y, "score", type: :quantitative)
+  """
+  @spec chart(VegaLite.t(), Table.Reader.t(), keyword()) :: VegaLite.t()
+  def chart(%Vl{} = vl, data, fields) do
+    cols = columns_for(data)
+    used_fields = used_fields(fields)
+    root = vl |> Vl.data_from_values(data, only: used_fields)
+    build_fields(fields, root, cols)
+  end
+
+  @spec chart(Table.Reader.t(), atom() | keyword(), keyword()) :: VegaLite.t()
+  def chart(data, mark, fields), do: chart(Vl.new(), data, mark, fields)
+
+  @doc """
+  Same as chart/3 but receives a valid `VegaLite` specification as a first argument.
+
+  ## Examples
+
+      data = [
+        %{"category" => "A", "score" => 28},
+        %{"category" => "B", "score" => 55}
+      ]
+
+      Vl.new(title: "With title")
+      |> Data.chart(data, :bar, x: "category", y: "score")
+
+      Vl.new(title: "With title")
+      |> Vl.mark(:bar)
+      |> Data.chart(data, x: "category", y: "score")
+
+  The above example achieves the same results as the example below.
+
+      Vl.new(title: "With title")
+      |> Vl.data_from_values(data, only: ["category", "score"])
+      |> Vl.mark(:bar)
+      |> Vl.encode_field(:x, "category", type: :nominal)
+      |> Vl.encode_field(:y, "score", type: :quantitative)
+  """
+  @spec chart(VegaLite.t(), Table.Reader.t(), atom() | keyword(), keyword()) :: VegaLite.t()
+  def chart(vl, data, mark, fields) do
+    cols = columns_for(data)
+    used_fields = used_fields(fields)
+    root = vl |> Vl.data_from_values(data, only: used_fields) |> encode_mark(mark)
+    build_fields(fields, root, cols)
+  end
+
+  @doc """
+  Returns the specification of a heat map for a given data and a list of fields to be encoded.
+
+  As a specialized chart, the heatmap expects an `:x` and `:y` and optionally a `:color` and a
+  `:text` field. Defaults to `:nominal` for the axes and `:quantitative` for color and text if
+  types are not specified.
+
+  ## Examples
+
+      data = [
+        %{"category" => "A", "score" => 28},
+        %{"category" => "B", "score" => 55}
+      ]
+
+      Data.heatmap(data, x: "category", y: "score", color: "score", text: "category")
+
+  """
+  @spec heatmap(Table.Reader.t(), keyword()) :: VegaLite.t()
+  def heatmap(data, fields), do: heatmap(Vl.new(), data, fields)
+
+  @doc """
+  Same as heatmap/2, but takes a valid `VegaLite` specification as the first argument.
+
+  ## Examples
+
+      data = [
+        %{"category" => "A", "score" => 28},
+        %{"category" => "B", "score" => 55}
+      ]
+
+      Vl.new(title: "Heatmap", width: 500)
+      |> Data.heatmap(data, x: "category", y: "score", color: "score", text: "category")
+  """
+  @spec heatmap(VegaLite.t(), Table.Reader.t(), keyword()) :: VegaLite.t()
+  def heatmap(vl, data, fields) do
+    fields = standardize_fields(fields) |> Enum.map(&heatmap_defaults/1)
+
+    case {fields[:x], fields[:y], fields[:text]} do
+      {nil, _, _} -> raise ArgumentError, "the x axis is required to plot a heatmap"
+      {_, nil, _} -> raise ArgumentError, "the y axis is required to plot a heatmap"
+      {_, _, nil} -> chart(vl, data, :rect, fields)
+      _ -> annotated_heatmap(vl, data, fields)
+    end
+  end
+
+  defp annotated_heatmap(vl, data, fields) do
+    text_fields = Keyword.take(fields, [:text, :x, :y])
+    used_fields = used_fields(fields)
+    rect_fields = Keyword.delete(fields, :text)
+
+    vl
+    |> Vl.data_from_values(data, only: used_fields)
+    |> Vl.layers([layer(data, :rect, rect_fields), layer(data, :text, text_fields)])
+  end
+
+  defp heatmap_defaults({field, opts}) when field in [:x, :y] do
+    {field, Keyword.put_new(opts, :type, :nominal)}
+  end
+
+  defp heatmap_defaults({field, opts}) when field in [:color, :text] do
+    {field, Keyword.put_new(opts, :type, :quantitative)}
+  end
+
+  defp encode_mark(vl, opts) when is_list(opts) do
+    {mark, opts} = Keyword.pop!(opts, :type)
+    Vl.mark(vl, mark, opts)
+  end
+
+  defp encode_mark(vl, mark) when is_atom(mark), do: Vl.mark(vl, mark)
+
+  defp encode_field(schema, cols, field, opts) do
+    {col, opts} = Keyword.pop!(opts, :field)
+    opts = Keyword.put_new_lazy(opts, :type, fn -> cols[col] end)
+    Vl.encode_field(schema, field, col, opts)
+  end
+
+  defp layer(data, mark, fields) do
+    cols = columns_for(data)
+    root = Vl.new() |> encode_mark(mark)
+    build_fields(fields, root, cols)
+  end
+
+  defp used_fields(fields) do
+    for {_key, field} <- fields, uniq: true do
+      if is_list(field), do: field[:field], else: field
+    end
+  end
+
+  defp build_fields(fields, root, cols) do
+    fields
+    |> standardize_fields()
+    |> Enum.reduce(root, fn {field, opts}, acc -> encode_field(acc, cols, field, opts) end)
+  end
+
+  defp standardize_fields(fields) do
+    for {field, opts} <- fields do
+      if is_list(opts), do: {field, opts}, else: {field, [field: opts]}
+    end
+  end
+
+  defp columns_for(data) do
+    with true <- implements?(Table.Reader, data),
+         data = {_, %{columns: [_ | _] = columns}, _} <- Table.Reader.init(data),
+         true <- Enum.all?(columns, &implements?(String.Chars, &1)) do
+      types = infer_types(data)
+      Enum.zip_with(columns, types, fn column, type -> {to_string(column), type} end) |> Map.new()
+    else
+      _ -> nil
+    end
+  end
+
+  defp implements?(protocol, value), do: protocol.impl_for(value) != nil
+
+  defp infer_types({:columns, %{columns: _columns}, data}) do
+    Enum.map(data, fn data -> data |> Enum.at(0) |> type_of() end)
+  end
+
+  defp infer_types({:rows, %{columns: columns}, data}) do
+    case Enum.fetch(data, 0) do
+      {:ok, row} -> Enum.map(row, &type_of/1)
+      :error -> Enum.map(columns, fn _ -> nil end)
+    end
+  end
+
+  defp type_of(%mod{}) when mod in [Decimal], do: :quantitative
+  defp type_of(%mod{}) when mod in [Date, NaiveDateTime, DateTime], do: :temporal
+
+  defp type_of(data) when is_number(data), do: :quantitative
+
+  defp type_of(data) when is_binary(data) do
+    if date?(data) or date_time?(data), do: :temporal, else: :nominal
+  end
+
+  defp type_of(data) when is_atom(data), do: :nominal
+
+  defp type_of(_), do: nil
+
+  defp date?(value), do: match?({:ok, _}, Date.from_iso8601(value))
+  defp date_time?(value), do: match?({:ok, _, _}, DateTime.from_iso8601(value))
+end
---
+++ test/vega_lite/data_test.exs
@@ -0,0 +1,309 @@
+defmodule VegaLite.DataTest do
+  use ExUnit.Case
+
+  alias VegaLite.Data
+  alias VegaLite, as: Vl
+
+  @data [
+    %{"height" => 170, "weight" => 80, "width" => 10, "unused" => "a"},
+    %{"height" => 190, "weight" => 85, "width" => 20, "unused" => "b"}
+  ]
+
+  describe "shorthand api" do
+    test "single field" do
+      vl =
+        Vl.new()
+        |> Vl.data_from_values(@data, only: ["height"])
+        |> Vl.mark(:bar)
+        |> Vl.encode_field(:x, "height", type: :quantitative)
+
+      assert vl == Data.chart(@data, :bar, x: "height")
+    end
+
+    test "multiple fields" do
+      vl =
+        Vl.new()
+        |> Vl.data_from_values(@data, only: ["height", "weight"])
+        |> Vl.mark(:bar)
+        |> Vl.encode_field(:x, "height", type: :quantitative)
+        |> Vl.encode_field(:y, "weight", type: :quantitative)
+
+      assert vl == Data.chart(@data, :bar, x: "height", y: "weight")
+    end
+
+    test "mark with options" do
+      vl =
+        Vl.new()
+        |> Vl.data_from_values(@data, only: ["height"])
+        |> Vl.mark(:point, line: true)
+        |> Vl.encode_field(:x, "height", type: :quantitative)
+
+      assert vl == Data.chart(@data, [type: :point, line: true], x: "height")
+    end
+
+    test "mark from pipe" do
+      vl =
+        Vl.new()
+        |> Vl.data_from_values(@data, only: ["height"])
+        |> Vl.mark(:point, line: true)
+        |> Vl.encode_field(:x, "height", type: :quantitative)
+
+      assert vl == Vl.new() |> Vl.mark(:point, line: true) |> Data.chart(@data, x: "height")
+    end
+
+    test "pipe to mark" do
+      vl =
+        Vl.new()
+        |> Vl.data_from_values(@data, only: ["height"])
+        |> Vl.encode_field(:x, "height", type: :quantitative)
+        |> Vl.mark(:point, line: true)
+
+      assert vl == Vl.new() |> Data.chart(@data, x: "height") |> Vl.mark(:point, line: true)
+      assert vl == Data.chart(@data, x: "height") |> Vl.mark(:point, line: true)
+    end
+
+    test "single field with options" do
+      vl =
+        Vl.new()
+        |> Vl.data_from_values(@data, only: ["height"])
+        |> Vl.mark(:bar)
+        |> Vl.encode_field(:x, "height", type: :nominal)
+
+      assert vl == Data.chart(@data, :bar, x: [field: "height", type: :nominal])
+    end
+
+    test "multiple fields with options" do
+      vl =
+        Vl.new()
+        |> Vl.data_from_values(@data, only: ["height", "weight"])
+        |> Vl.mark(:bar)
+        |> Vl.encode_field(:x, "height", type: :nominal)
+        |> Vl.encode_field(:y, "weight", type: :nominal)
+
+      assert vl ==
+               Data.chart(@data, :bar,
+                 x: [field: "height", type: :nominal],
+                 y: [field: "weight", type: :nominal]
+               )
+    end
+
+    test "nested field options" do
+      vl =
+        Vl.new()
+        |> Vl.data_from_values(@data, only: ["height"])
+        |> Vl.mark(:bar)
+        |> Vl.encode_field(:x, "height", type: :quantitative)
+        |> Vl.encode_field(:color, "height", type: :quantitative, scale: [scheme: "category10"])
+
+      assert vl ==
+               Data.chart(@data, :bar,
+                 x: "height",
+                 color: [field: "height", type: :quantitative, scale: [scheme: "category10"]]
+               )
+    end
+
+    test "piped from VegaLite" do
+      vl =
+        Vl.new(title: "With title")
+        |> Vl.data_from_values(@data, only: ["height"])
+        |> Vl.mark(:point)
+        |> Vl.encode_field(:x, "height", type: :quantitative)
+
+      assert vl == Vl.new(title: "With title") |> Data.chart(@data, :point, x: "height")
+    end
+
+    test "piped into VegaLite" do
+      vl =
+        Vl.new()
+        |> Vl.data_from_values(@data, only: ["height"])
+        |> Vl.mark(:point)
+        |> Vl.encode_field(:x, "height", type: :quantitative)
+        |> Vl.encode_field(:color, "height", type: :quantitative, scale: [scheme: "category10"])
+
+      assert vl ==
+               Data.chart(@data, :point, x: "height")
+               |> Vl.encode_field(:color, "height",
+                 type: :quantitative,
+                 scale: [scheme: "category10"]
+               )
+    end
+
+    test "piped from and into VegaLite" do
+      vl =
+        Vl.new(title: "With title")
+        |> Vl.data_from_values(@data, only: ["height"])
+        |> Vl.mark(:point)
+        |> Vl.encode_field(:x, "height", type: :quantitative)
+        |> Vl.encode_field(:color, "height", type: :quantitative, scale: [scheme: "category10"])
+
+      assert vl ==
+               Vl.new(title: "With title")
+               |> Data.chart(@data, :point, x: "height")
+               |> Vl.encode_field(:color, "height",
+                 type: :quantitative,
+                 scale: [scheme: "category10"]
+               )
+    end
+
+    test "combined with layers" do
+      vl =
+        Vl.new(title: "Heatmap")
+        |> Vl.layers([
+          Vl.new()
+          |> Vl.data_from_values(@data, only: ["height", "weight"])
+          |> Vl.mark(:rect)
+          |> Vl.encode_field(:x, "height", type: :nominal)
+          |> Vl.encode_field(:y, "weight", type: :nominal)
+          |> Vl.encode_field(:color, "height", type: :quantitative),
+          Vl.new()
+          |> Vl.data_from_values(@data, only: ["height", "weight"])
+          |> Vl.mark(:text)
+          |> Vl.encode_field(:x, "height", type: :nominal)
+          |> Vl.encode_field(:y, "weight", type: :nominal)
+          |> Vl.encode_field(:text, "height", type: :quantitative)
+        ])
+
+      sh =
+        Vl.new(title: "Heatmap")
+        |> Vl.layers([
+          Data.chart(@data, :rect,
+            x: [field: "height", type: :nominal],
+            y: [field: "weight", type: :nominal],
+            color: "height"
+          ),
+          Data.chart(@data, :text,
+            x: [field: "height", type: :nominal],
+            y: [field: "weight", type: :nominal],
+            text: "height"
+          )
+        ])
+
+      assert vl == sh
+    end
+  end
+
+  describe "heatmap" do
+    test "simple heatmap" do
+      vl =
+        Vl.new()
+        |> Vl.data_from_values(@data, only: ["height", "weight"])
+        |> Vl.mark(:rect)
+        |> Vl.encode_field(:x, "height", type: :nominal)
+        |> Vl.encode_field(:y, "weight", type: :nominal)
+
+      assert vl == Data.heatmap(@data, x: "height", y: "weight")
+    end
+
+    test "simple heatmap with color" do
+      vl =
+        Vl.new()
+        |> Vl.data_from_values(@data, only: ["height", "weight"])
+        |> Vl.mark(:rect)
+        |> Vl.encode_field(:x, "height", type: :nominal)
+        |> Vl.encode_field(:y, "weight", type: :nominal)
+        |> Vl.encode_field(:color, "height", type: :quantitative)
+
+      assert vl == Data.heatmap(@data, x: "height", y: "weight", color: "height")
+    end
+
+    test "heatmap with color and text" do
+      vl =
+        Vl.new()
+        |> Vl.data_from_values(@data, only: ["height", "weight"])
+        |> Vl.layers([
+          Vl.new()
+          |> Vl.mark(:rect)
+          |> Vl.encode_field(:x, "height", type: :nominal)
+          |> Vl.encode_field(:y, "weight", type: :nominal)
+          |> Vl.encode_field(:color, "height", type: :quantitative),
+          Vl.new()
+          |> Vl.mark(:text)
+          |> Vl.encode_field(:x, "height", type: :nominal)
+          |> Vl.encode_field(:y, "weight", type: :nominal)
+          |> Vl.encode_field(:text, "height", type: :quantitative)
+        ])
+
+      assert vl == Data.heatmap(@data, x: "height", y: "weight", color: "height", text: "height")
+    end
+
+    test "heatmap with title" do
+      vl =
+        Vl.new(title: "Heatmap")
+        |> Vl.data_from_values(@data, only: ["height", "weight"])
+        |> Vl.layers([
+          Vl.new()
+          |> Vl.mark(:rect)
+          |> Vl.encode_field(:x, "height", type: :nominal)
+          |> Vl.encode_field(:y, "weight", type: :nominal)
+          |> Vl.encode_field(:color, "height", type: :quantitative),
+          Vl.new()
+          |> Vl.mark(:text)
+          |> Vl.encode_field(:x, "height", type: :nominal)
+          |> Vl.encode_field(:y, "weight", type: :nominal)
+          |> Vl.encode_field(:text, "height", type: :quantitative)
+        ])
+
+      assert vl ==
+               Vl.new(title: "Heatmap")
+               |> Data.heatmap(@data, x: "height", y: "weight", color: "height", text: "height")
+    end
+
+    test "heatmap with specified types" do
+      vl =
+        Vl.new()
+        |> Vl.data_from_values(@data, only: ["height", "weight"])
+        |> Vl.layers([
+          Vl.new()
+          |> Vl.mark(:rect)
+          |> Vl.encode_field(:x, "height", type: :quantitative)
+          |> Vl.encode_field(:y, "weight", type: :quantitative)
+          |> Vl.encode_field(:color, "height", type: :nominal),
+          Vl.new()
+          |> Vl.mark(:text)
+          |> Vl.encode_field(:x, "height", type: :quantitative)
+          |> Vl.encode_field(:y, "weight", type: :quantitative)
+          |> Vl.encode_field(:text, "height", type: :quantitative)
+        ])
+
+      assert vl ==
+               Data.heatmap(@data,
+                 x: [field: "height", type: :quantitative],
+                 y: [field: "weight", type: :quantitative],
+                 color: [field: "height", type: :nominal],
+                 text: "height"
+               )
+    end
+
+    test "heatmap with a text field different from the axes" do
+      vl =
+        Vl.new()
+        |> Vl.data_from_values(@data, only: ["height", "weight", "width"])
+        |> Vl.layers([
+          Vl.new()
+          |> Vl.mark(:rect)
+          |> Vl.encode_field(:x, "height", type: :nominal)
+          |> Vl.encode_field(:y, "weight", type: :nominal)
+          |> Vl.encode_field(:color, "height", type: :quantitative),
+          Vl.new()
+          |> Vl.mark(:text)
+          |> Vl.encode_field(:x, "height", type: :nominal)
+          |> Vl.encode_field(:y, "weight", type: :nominal)
+          |> Vl.encode_field(:text, "width", type: :quantitative)
+        ])
+
+      assert vl == Data.heatmap(@data, x: "height", y: "weight", color: "height", text: "width")
+    end
+
+    test "raises an error when the x axis is not given" do
+      assert_raise ArgumentError, "the x axis is required to plot a heatmap", fn ->
+        Data.heatmap(@data, y: "y")
+      end
+    end
+
+    test "raises an error when the y axis is not given" do
+      assert_raise ArgumentError, "the y axis is required to plot a heatmap", fn ->
+        Data.heatmap(@data, x: "x", text: "text")
+      end
+    end
+  end
+end
