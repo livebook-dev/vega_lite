937c08008281c187d13ca30b72211afa81766c43
Alex Koutmos <akoutmos@gmail.com>
2024-11-05 18:00:18 -0500

    Cleaning up code

--- lib/vega_lite/native.ex
+++ lib/vega_lite/native.ex
@@ -14,18 +14,24 @@ defmodule VegaLite.Native do
       Enum.uniq(["aarch64-unknown-linux-musl" | RustlerPrecompiled.Config.default_targets()]),
     force_build: System.get_env("VEGA_LITE_BUILD") in ["1", "true"]
 
-  # New
+  # TODO: Implement the rest of these
+
+  # Vega related NIF functions
   # def vega_to_html(), do: :erlang.nif_error(:nif_not_loaded)
   # def vega_to_jpeg(), do: :erlang.nif_error(:nif_not_loaded)
   # def vega_to_pdf(), do: :erlang.nif_error(:nif_not_loaded)
   # def vega_to_png(), do: :erlang.nif_error(:nif_not_loaded)
-  # def vega_to_scenegraph(), do: :erlang.nif_error(:nif_not_loaded)
   # def vega_to_svg(), do: :erlang.nif_error(:nif_not_loaded)
+  # def vega_to_scenegraph(), do: :erlang.nif_error(:nif_not_loaded)
+
+  # VegaLite related NIF functions
   # def vegalite_to_html(), do: :erlang.nif_error(:nif_not_loaded)
   # def vegalite_to_jpeg(), do: :erlang.nif_error(:nif_not_loaded)
   # def vegalite_to_pdf(), do: :erlang.nif_error(:nif_not_loaded)
   # def vegalite_to_png(), do: :erlang.nif_error(:nif_not_loaded)
+  def vegalite_to_svg(_vl_json_spec), do: :erlang.nif_error(:nif_not_loaded)
   # def vegalite_to_scenegraph(), do: :erlang.nif_error(:nif_not_loaded)
-  def vegalite_to_svg(_vl_json_spec, _version), do: :erlang.nif_error(:nif_not_loaded)
+
+  # Conversion related NIF functions
   # def vegalite_to_vega(), do: :erlang.nif_error(:nif_not_loaded)
 end
--- native/ex_vl_convert/src/lib.rs
+++ native/ex_vl_convert/src/lib.rs
@@ -4,7 +4,7 @@ use rustler::NifTuple;
 use vl_convert_rs::converter::VlOpts;
 use vl_convert_rs::{VlConverter, VlVersion};
 
-use webp::{Encoder, WebPMemory};
+// use webp::{Encoder, WebPMemory};
 
 mod atoms {
     rustler::atoms! {
@@ -23,6 +23,27 @@ struct StringResultTuple {
 struct BinaryResultTuple {
     lhs: Atom,
     rhs: Vec<u8>,
+}
+
+fn ok_string_tuple(data: String) -> StringResultTuple {
+    return StringResultTuple {
+        lhs: atoms::ok(),
+        rhs: data,
+    };
+}
+
+fn error_tuple(error: String) -> StringResultTuple {
+    return StringResultTuple {
+        lhs: atoms::error(),
+        rhs: error.to_string(),
+    };
+}
+
+fn vl_opts() -> VlOpts {
+    return VlOpts {
+        vl_version: VlVersion::v5_20,
+        ..Default::default()
+    };
 }
 
 // #[rustler::nif(schedule = "DirtyCpu")]
@@ -86,30 +107,18 @@ struct BinaryResultTuple {
 // }
 
 #[rustler::nif(schedule = "DirtyCpu")]
-fn vegalite_to_svg(vega_lite_spec: String, version: String) -> StringResultTuple {
-    let mut converter = VlConverter::new();
-
-    let vl_spec: serde_json::Value = serde_json::from_str(vega_lite_spec.as_str()).unwrap();
-
-    let vl_version = match version.as_str() {
-        "5.16" => VlVersion::v5_16,
-        "5.17" => VlVersion::v5_17,
-        "5.18" => VlVersion::v5_18,
-        _ => VlVersion::v5_19,
+fn vegalite_to_svg(vega_lite_spec: String) -> StringResultTuple {
+    let vl_spec: serde_json::Value = match serde_json::from_str(vega_lite_spec.as_str()) {
+        Ok(spec) => spec,
+        Err(_err) => return error_tuple("VegaLite spec is not valid JSON".to_string()),
     };
 
-    let svg = futures::executor::block_on(converter.vegalite_to_svg(
-        vl_spec,
-        VlOpts {
-            vl_version: vl_version,
-            ..Default::default()
-        },
-    ))
-    .expect("Failed to perform Vega-Lite to Vega conversion");
+    let mut converter = VlConverter::new();
+    let svg_result = futures::executor::block_on(converter.vegalite_to_svg(vl_spec, vl_opts()));
 
-    return StringResultTuple {
-        lhs: atoms::ok(),
-        rhs: svg,
+    return match svg_result {
+        Ok(svg) => ok_string_tuple(svg),
+        Err(err) => error_tuple(err.to_string()),
     };
 }
 
