1ad8e6a4d84d117efcfd4d9e42c06fd7d291c3bd
Alex Koutmos <akoutmos@gmail.com>
2024-11-05 15:48:21 -0500

    Updated versions and kept vl-convert-rs on 1.6 as 1.7 runs into a compiler error

--- lib/vega_lite/native.ex
+++ lib/vega_lite/native.ex
@@ -14,11 +14,6 @@ defmodule VegaLite.Native do
       Enum.uniq(["aarch64-unknown-linux-musl" | RustlerPrecompiled.Config.default_targets()]),
     force_build: System.get_env("VEGA_LITE_BUILD") in ["1", "true"]
 
-  # Old
-  # def to_jpeg(_vl_json_spec, _version, _scale), do: :erlang.nif_error(:nif_not_loaded)
-  # def to_png(_vl_json_spec, _version, _scale), do: :erlang.nif_error(:nif_not_loaded)
-  # def to_svg(_vl_json_spec, _version), do: :erlang.nif_error(:nif_not_loaded)
-
   # New
   # def vega_to_html(), do: :erlang.nif_error(:nif_not_loaded)
   # def vega_to_jpeg(), do: :erlang.nif_error(:nif_not_loaded)
--- native/ex_vl_convert/src/lib.rs
+++ native/ex_vl_convert/src/lib.rs
@@ -25,65 +25,65 @@ struct BinaryResultTuple {
     rhs: Vec<u8>,
 }
 
-#[rustler::nif(schedule = "DirtyCpu")]
-fn to_jpeg(vega_lite_spec: String, version: String, scale: f32) -> BinaryResultTuple {
-    let mut converter = VlConverter::new();
-
-    let vl_spec: serde_json::Value = serde_json::from_str(vega_lite_spec.as_str()).unwrap();
-
-    let vl_version = match version.as_str() {
-        "5.16" => VlVersion::v5_16,
-        "5.17" => VlVersion::v5_17,
-        "5.18" => VlVersion::v5_18,
-        _ => VlVersion::v5_19,
-    };
-
-    let jpeg = futures::executor::block_on(converter.vegalite_to_jpeg(
-        vl_spec,
-        VlOpts {
-            vl_version: vl_version,
-            ..Default::default()
-        },
-        Some(scale),
-        None,
-    ))
-    .expect("Failed to perform Vega-Lite to Vega conversion");
-
-    return BinaryResultTuple {
-        lhs: atoms::ok(),
-        rhs: jpeg,
-    };
-}
-
-#[rustler::nif(schedule = "DirtyCpu")]
-fn to_png(vega_lite_spec: String, version: String, scale: f32) -> BinaryResultTuple {
-    let mut converter = VlConverter::new();
-
-    let vl_spec: serde_json::Value = serde_json::from_str(vega_lite_spec.as_str()).unwrap();
-
-    let vl_version = match version.as_str() {
-        "5.16" => VlVersion::v5_16,
-        "5.17" => VlVersion::v5_17,
-        "5.18" => VlVersion::v5_18,
-        _ => VlVersion::v5_19,
-    };
-
-    let png = futures::executor::block_on(converter.vegalite_to_png(
-        vl_spec,
-        VlOpts {
-            vl_version: vl_version,
-            ..Default::default()
-        },
-        Some(scale),
-        None,
-    ))
-    .expect("Failed to perform Vega-Lite to Vega conversion");
-
-    return BinaryResultTuple {
-        lhs: atoms::ok(),
-        rhs: png,
-    };
-}
+// #[rustler::nif(schedule = "DirtyCpu")]
+// fn to_jpeg(vega_lite_spec: String, version: String, scale: f32) -> BinaryResultTuple {
+//     let mut converter = VlConverter::new();
+//
+//     let vl_spec: serde_json::Value = serde_json::from_str(vega_lite_spec.as_str()).unwrap();
+//
+//     let vl_version = match version.as_str() {
+//         "5.16" => VlVersion::v5_16,
+//         "5.17" => VlVersion::v5_17,
+//         "5.18" => VlVersion::v5_18,
+//         _ => VlVersion::v5_19,
+//     };
+//
+//     let jpeg = futures::executor::block_on(converter.vegalite_to_jpeg(
+//         vl_spec,
+//         VlOpts {
+//             vl_version: vl_version,
+//             ..Default::default()
+//         },
+//         Some(scale),
+//         None,
+//     ))
+//     .expect("Failed to perform Vega-Lite to Vega conversion");
+//
+//     return BinaryResultTuple {
+//         lhs: atoms::ok(),
+//         rhs: jpeg,
+//     };
+// }
+//
+// #[rustler::nif(schedule = "DirtyCpu")]
+// fn to_png(vega_lite_spec: String, version: String, scale: f32) -> BinaryResultTuple {
+//     let mut converter = VlConverter::new();
+//
+//     let vl_spec: serde_json::Value = serde_json::from_str(vega_lite_spec.as_str()).unwrap();
+//
+//     let vl_version = match version.as_str() {
+//         "5.16" => VlVersion::v5_16,
+//         "5.17" => VlVersion::v5_17,
+//         "5.18" => VlVersion::v5_18,
+//         _ => VlVersion::v5_19,
+//     };
+//
+//     let png = futures::executor::block_on(converter.vegalite_to_png(
+//         vl_spec,
+//         VlOpts {
+//             vl_version: vl_version,
+//             ..Default::default()
+//         },
+//         Some(scale),
+//         None,
+//     ))
+//     .expect("Failed to perform Vega-Lite to Vega conversion");
+//
+//     return BinaryResultTuple {
+//         lhs: atoms::ok(),
+//         rhs: png,
+//     };
+// }
 
 #[rustler::nif(schedule = "DirtyCpu")]
 fn vegalite_to_svg(vega_lite_spec: String, version: String) -> StringResultTuple {
@@ -113,38 +113,38 @@ fn vegalite_to_svg(vega_lite_spec: String, version: String) -> StringResultTuple
     };
 }
 
-#[rustler::nif(schedule = "DirtyCpu")]
-fn to_webp(vega_lite_spec: String, version: String, scale: f32) -> BinaryResultTuple {
-    let mut converter = VlConverter::new();
+// #[rustler::nif(schedule = "DirtyCpu")]
+// fn to_webp(vega_lite_spec: String, version: String, scale: f32) -> BinaryResultTuple {
+//     let mut converter = VlConverter::new();
+//
+//     let vl_spec: serde_json::Value = serde_json::from_str(vega_lite_spec.as_str()).unwrap();
+//
+//     let vl_version = match version.as_str() {
+//         "5.16" => VlVersion::v5_16,
+//         "5.17" => VlVersion::v5_17,
+//         "5.18" => VlVersion::v5_18,
+//         _ => VlVersion::v5_19,
+//     };
+//
+//     let png = futures::executor::block_on(converter.vegalite_to_png(
+//         vl_spec,
+//         VlOpts {
+//             vl_version: vl_version,
+//             ..Default::default()
+//         },
+//         Some(scale),
+//         None,
+//     ))
+//     .expect("Failed to perform Vega-Lite to Vega conversion");
+//
+//     let png_image = image::load_from_memory(&png).unwrap();
+//     let encoder: Encoder = Encoder::from_image(&png_image).unwrap();
+//     let webp: WebPMemory = encoder.encode(65f32);
+//
+//     return BinaryResultTuple {
+//         lhs: atoms::ok(),
+//         rhs: Vec::from(&*webp),
+//     };
+// }
 
-    let vl_spec: serde_json::Value = serde_json::from_str(vega_lite_spec.as_str()).unwrap();
-
-    let vl_version = match version.as_str() {
-        "5.16" => VlVersion::v5_16,
-        "5.17" => VlVersion::v5_17,
-        "5.18" => VlVersion::v5_18,
-        _ => VlVersion::v5_19,
-    };
-
-    let png = futures::executor::block_on(converter.vegalite_to_png(
-        vl_spec,
-        VlOpts {
-            vl_version: vl_version,
-            ..Default::default()
-        },
-        Some(scale),
-        None,
-    ))
-    .expect("Failed to perform Vega-Lite to Vega conversion");
-
-    let png_image = image::load_from_memory(&png).unwrap();
-    let encoder: Encoder = Encoder::from_image(&png_image).unwrap();
-    let webp: WebPMemory = encoder.encode(65f32);
-
-    return BinaryResultTuple {
-        lhs: atoms::ok(),
-        rhs: Vec::from(&*webp),
-    };
-}
-
-rustler::init!("Elixir.VegaLite.Native", [vegalite_to_svg]);
+rustler::init!("Elixir.VegaLite.Native");
