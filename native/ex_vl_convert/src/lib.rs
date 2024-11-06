203d9f23a86cad595fae08c6f5d311dca730b069
Alex Koutmos <akoutmos@gmail.com>
2024-11-05 19:43:24 -0500

    Clean up

--- native/ex_vl_convert/src/lib.rs
+++ native/ex_vl_convert/src/lib.rs
@@ -9,7 +9,9 @@ use vl_convert_rs::converter::VlOpts;
 use vl_convert_rs::VlConverter;
 use vl_convert_rs::VlVersion;
 
-// use webp::{Encoder, WebPMemory};
+// +-------------------------------------+
+// |        Rustler Return Types         |
+// +-------------------------------------+
 
 mod atoms {
     rustler::atoms! {
@@ -239,38 +241,3 @@ fn vl_opts() -> VlOpts {
 }
 
 rustler::init!("Elixir.VegaLite.Native");
-
-// TODO: Clean this up once things get migrated over
-// #[rustler::nif(schedule = "DirtyCpu")]
-// fn to_webp(vega_lite_spec: String, version: String, scale: f32) -> BinaryResultTuple {
-//     let mut converter = VlConverter::new();
-//
-//     let vl_spec: serde_json::Value = serde_json::from_str(vega_lite_spec.as_str()).unwrap();
-//
-//     let vl_version = match version.as_str() {
-//         "5.16" => VlVersion::v5_16,
-//         "5.17" => VlVersion::v5_17,
-//         "5.18" => VlVersion::v5_18,
-//         _ => VlVersion::v5_19,
-//     };
-//
-//     let png = futures::executor::block_on(converter.vegalite_to_png(
-//         vl_spec,
-//         VlOpts {
-//             vl_version: vl_version,
-//             ..Default::default()
-//         },
-//         Some(scale),
-//         None,
-//     ))
-//     .expect("Failed to perform Vega-Lite to Vega conversion");
-//
-//     let png_image = image::load_from_memory(&png).unwrap();
-//     let encoder: Encoder = Encoder::from_image(&png_image).unwrap();
-//     let webp: WebPMemory = encoder.encode(65f32);
-//
-//     return BinaryResultTuple {
-//         lhs: atoms::ok(),
-//         rhs: Vec::from(&*webp),
-//     };
-// }
